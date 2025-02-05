4ef162386cb4e2c822209a8c2baf5b5e7c2383c3
Toss2204 <izotov7428@gmail.com>
2025-02-05 18:16:06 +0300

    Add project files.

---
+++ FileClass.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_OTUS_Files
+{
+    public class FileClass
+    {
+       public int _countFiles;
+        public string _nameFirstFile;
+        public string _pathDirectory;
+
+
+
+        public async Task CreateFiles(int countFiles, string nameFirstFile, string pathDirectory)
+        {
+
+            if (countFiles == 0)
+            {
+                _countFiles = 1;
+            }
+            else
+            {
+                _countFiles = countFiles;
+            }
+            
+
+            _nameFirstFile = nameFirstFile==null?"": nameFirstFile;
+            _pathDirectory = pathDirectory;
+
+            string expansion = ".txt";
+
+            for (int i = 0; i < countFiles; i++)
+            {
+                string newFileName = _nameFirstFile;
+                
+                    int  counter=i+1;
+                    string counterStr=counter.ToString();
+
+                    newFileName = string.Concat(string.Concat(_nameFirstFile, counterStr), expansion);
+
+                    var path= Path.Combine(_pathDirectory, newFileName);
+
+                    if (File.Exists(path) is false) 
+                    {
+                        try
+                        {
+                            await using var stream = File.Create(path);
+
+                            Console.WriteLine($"Создан файл {newFileName}{expansion}");
+                        }
+                        catch (Exception ex)
+                        {
+
+                            Console.WriteLine(ex.Message);
+                        } 
+                      
+
+                    }
+
+
+             }
+        
+        }
+
+        public async Task WriteNameFiles(string pathDirectory) 
+        {
+            var directory = new DirectoryInfo(pathDirectory);
+
+            FileInfo[] files = directory.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                try
+                {
+
+                File.WriteAllText(file.FullName, file.Name, Encoding.UTF8);
+                Console.WriteLine($"Записали имя файла {file.Name}");
+
+                string currentDateTime = $"\n{DateTime.Now.ToString("dd MMMM yyyy|HH:mm:ss")}";
+                File.AppendAllText(file.FullName, currentDateTime, Encoding.UTF8);
+                Console.WriteLine($"Записали дату в файл {file.Name}");
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+
+                    Console.WriteLine(ex.Message); ;
+                }
+
+            }
+        }
+
+        public async Task ReadFiles(string pathDirectory)
+        {
+            var directory = new DirectoryInfo(pathDirectory);
+
+            Console.WriteLine();
+            FileInfo[] files = directory.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine($"Читаем файл {file.Name}");
+                string fileText = File.ReadAllText(file.FullName);
+                Console.WriteLine($" {fileText}");
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+
+namespace DZ_OTUS_Files
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+
+            List<string> pathes=new List<string>() { @"C:\Otus\TestDir1", @"C:\Otus\TestDir2" };
+            //string path1 = @"C:\Otus\TestDir1";
+            //string path2 = @"C:\Otus\TestDir2";
+
+            foreach (string path in pathes)
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+                if (!dirInfo.Exists)
+                {
+                    dirInfo.Create();
+                    Console.WriteLine($"Создана директория: {path}");
+                }
+                else
+                {
+                    Console.WriteLine($"Директория: {path} уже существует!");
+                }
+
+
+                FileClass newFileCreator = new FileClass();
+                var result = newFileCreator.CreateFiles(10, "File_example", path);
+
+                
+            }
+
+            //await Task.Delay(2000);
+
+            Console.WriteLine();
+            Console.WriteLine(new string('-', Console.WindowWidth));
+
+            FileClass newFileCreator2 = new FileClass();
+
+            foreach (string path in pathes)
+            {
+
+                await newFileCreator2.WriteNameFiles(path);
+
+
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine(new string('-', Console.WindowWidth));
+
+            foreach (string path in pathes)
+            {
+
+                await newFileCreator2.ReadFiles(path);
+
+
+            }
+        }
+    }
+}
